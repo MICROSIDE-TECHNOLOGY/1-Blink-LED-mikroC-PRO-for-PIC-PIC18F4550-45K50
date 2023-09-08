4774f564aa9b460820f8b2092ae6fc6e302295b7
vhperaltaj <victor.peralta@microside.com>
2023-09-08 10:27:05 -0600

    Fixed indentation

--- Proyecto/45K50/blink_led.c
+++ Proyecto/45K50/blink_led.c
@@ -16,22 +16,21 @@ Para usar el código con bootloader, configurar como lo indica MICROSIDE:
 2) Project> Edit Project> Insertar 48.000000 en "MCU Clock Frequency [MHz]" box
 ********************************************************************************/
 
-#pragma orgall 0x1FFF           //Espacio reservado para bootloader
+#pragma orgall 0x1FFF   // Espacio reservado para bootloader
 
-#define LED LATA4_bit           //Se crea una definición donde se almacena el registro del pin
-                                //Tome en cuenta que en versiones anteriores se hacia uso del pin A1
+#define LED LATA4_bit   // Se crea una definición donde se almacena el registro del pin
+                        // Tome en cuenta que en versiones anteriores se hacia uso del pin A1
 
-void main() org 0x2000          //Se especifica el segmento a partir de cual se almacenara el programa ya que existe un espacio
+void main() org 0x2000  // Se especifica el segmento a partir de cual se almacenara el programa ya que existe un espacio
 {
-     LATA4_bit = 0;             //Se asigna el pin como salida
-     TRISA4_bit = 0;            //Se establece el pin en un estado bajo
-     ANSELA = 0;                //Se configura el pin como salida digital
+   LATA4_bit = 0;       // Se asigna el pin como salida
+   TRISA4_bit = 0;      // Se establece el pin en un estado bajo
+   ANSELA = 0;          // Se configura el pin como salida digital
 
-     while(1)
-     {
-             LED = 1;           //Se establece como estado alto el pin digital
-             Delay_ms(1000);    //Se establece un espera de 1 segundo
-             LED = 0;           //Se establece como estado bajo el pin digital
-             Delay_ms(1000);    //Se establece un espera de 1 segundo
-     }
+   while ( 1 ) {
+      LED = 1;          // Se establece como estado alto el pin digital
+      Delay_ms( 1000 ); // Se establece un espera de 1 segundo
+      LED = 0;          // Se establece como estado bajo el pin digital
+      Delay_ms( 1000 ); // Se establece un espera de 1 segundo
+   }
 }
